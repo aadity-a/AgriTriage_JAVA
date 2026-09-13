216e4e16fceb0bf773f04515853f136f19c72de0
aadity-a <aadi042105@gmail.com>
2026-09-14 03:18:52 +0530

    feat(core): initialize Spring Boot AgriTriageApplication entrypoint

---
+++ src/main/java/com/agritriage/AgriTriageApplication.java
@@ -0,0 +1,12 @@
+package com.agritriage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgriTriageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgriTriageApplication.class, args);
+    }
+}
