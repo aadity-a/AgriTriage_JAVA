92fd0c6f14000a0f60c2aba8984990d552ca3f81
aadity-a <aadi042105@gmail.com>
2026-09-14 03:18:56 +0530

    test: add AgriTriageApplicationTests context load test

---
+++ src/test/java/com/agritriage/AgriTriageApplicationTests.java
@@ -0,0 +1,12 @@
+package com.agritriage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgriTriageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
